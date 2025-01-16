6ade9c8008547f2deeacdedd4cf3b20a9f2b5a5e
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2025-01-16 08:24:33 -0300

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://localhost:4000/api/auth/login', {
+            const response = await fetch('https://api-ckry.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
