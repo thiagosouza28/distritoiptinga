4b8e1be752afe46abeea5038adbefd7d9b405258
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2025-01-19 16:16:09 -0300

    Update inscricao.js

--- src/js/inscricao.js
+++ src/js/inscricao.js
@@ -81,7 +81,7 @@ document.getElementById('inscricaoForm').addEventListener('submit', async functi
 
      showProcessingOverlay();
     try {
-        const response = await fetch('https://api-ckry.onrender.com/api/participantes/public', {
+        const response = await fetch('https://api-ckry.onrender.com/api/participantes/inscricao', {
             method: 'POST',
            headers: {
                 'Content-Type': 'application/json',
